33bcfbb5717b7c888e35f4903320e35a9f9ae5aa
Artem <artmh@proton.me>
2024-01-26 17:39:02 +0200

    feat: android support

--- android/src/main/java/com/expofp/ExpofpViewManager.kt
+++ android/src/main/java/com/expofp/ExpofpViewManager.kt
@@ -1,16 +1,33 @@
 package com.expofp
 
 import android.graphics.Color
+import android.telecom.Call.Details
 import android.view.View
+import com.expofp.fplan.FplanEventsListener
+import com.expofp.fplan.FplanView
+import com.expofp.fplan.Settings
+import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
+import okhttp3.Route
+
+
+
 
 class ExpofpViewManager : SimpleViewManager<View>() {
   override fun getName() = "ExpofpView"
 
   override fun createViewInstance(reactContext: ThemedReactContext): View {
-    return View(reactContext)
+    var settings: Settings = Settings();
+
+    var view = FplanView(reactContext)
+    view.init("https://demo.expofp.com", settings);
+    return view;
+  }
+  @ReactProp(name = "url")
+  fun setUrl(view: FplanView, string: String?) {
+    view.init(string)
   }
 
   @ReactProp(name = "color")
