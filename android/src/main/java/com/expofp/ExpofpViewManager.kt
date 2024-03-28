d353a131e72aef4edcf2f381710255531ffc319c
Arystan <arystankaliakparov@gmail.com>
2024-03-28 13:51:49 +0500

    add crowdconnected

--- android/src/main/java/com/expofp/ExpofpViewManager.kt
+++ android/src/main/java/com/expofp/ExpofpViewManager.kt
@@ -1,37 +1,55 @@
 package com.expofp
 
+import android.app.Application
 import android.graphics.Color
 import android.telecom.Call.Details
 import android.view.View
 import com.expofp.fplan.FplanEventsListener
 import com.expofp.fplan.FplanView
-import com.expofp.fplan.Settings
 import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
 import okhttp3.Route
-
-
-
+import android.util.Log
+import com.facebook.react.bridge.ReadableMap
+import android.widget.Toast
+import com.expofp.common.GlobalLocationProvider
+import com.expofp.crowdconnected.Settings
+import com.expofp.crowdconnected.CrowdConnectedProvider
+import com.expofp.crowdconnected.Mode
 
 class ExpofpViewManager : SimpleViewManager<View>() {
+  private var reactContext: ThemedReactContext? = null
+
   override fun getName() = "ExpofpView"
 
   override fun createViewInstance(reactContext: ThemedReactContext): View {
-    var settings: Settings = Settings();
-
-    var view = FplanView(reactContext)
-
-    return view;
+      this.reactContext = reactContext
+      var view = FplanView(reactContext)
+      
+      return view;
   }
+
   @ReactProp(name = "url")
   fun setUrl(view: FplanView, string: String?) {
-    view.init(string)
+    view.init(string);
   }
 
-  @ReactProp(name = "color")
-  fun setColor(view: View, color: String) {
-    view.setBackgroundColor(Color.parseColor(color))
+  @ReactProp(name = "crowdConnectedSettings")
+  fun setCrowdConnectedSettings(view: FplanView, settingsMap: ReadableMap?) {
+      val context = reactContext?.applicationContext ?: return
+      val application = context as? Application ?: return
+      settingsMap?.let {
+          val lpSettings = Settings(
+              it.getString("appKey") ?: "",
+              it.getString("token") ?: "",
+              it.getString("secret") ?: "",
+              Mode.IPS_AND_GPS
+          )
+          val locationProvider = CrowdConnectedProvider(application, lpSettings)
+          GlobalLocationProvider.init(locationProvider)
+          GlobalLocationProvider.start()
+      }
   }
 }
